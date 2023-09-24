1b5c81c8db34a2a49caaeed21475285247dcd57e
lloydmuncal <jlloydmuncal@gmail.com>
2023-09-24 08:43:46 +0800

    first commit

--- src/components/Home/ProjectsPage.vue
+++ src/components/Home/ProjectsPage.vue
@@ -10,14 +10,14 @@
           </div>
         </div>
         <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 px-2">
-        <a :href="project.demoUrl" target="_blank" rel="noopener noreferrer">
+        <a :href="project.codeUrl" target="_blank" rel="noopener noreferrer">
           <div class="bg-gray-400 rounded-lg shadow-lg p-6 cursor-pointer ">
             <div class="mb-4">
               <h2 class="text-2xl text-white font-semibold">{{ project.title }}</h2>
               <p class="text-white">{{ project.description }}</p>
             </div>
             <div class="mb-4">
-              <img :src="project.imageUrl" alt="Project Image" class="rounded-lg" />
+              <img src="../../assets/image/mycv.PNG" alt="Project Image" class="rounded-lg" />
             </div>
           </div>
         </a>
@@ -29,13 +29,11 @@
   
   <script setup>
   import { ref } from 'vue';
-  
   const project = ref({
-    title: 'Employee Management System',
-    description: 'Mama mo CRUD.',
-    imageUrl: 'https://user-images.githubusercontent.com/48409548/94567114-8aa5ea80-0284-11eb-99f6-87401b099848.png',
-    demoUrl: 'https://example.com/demo',
-    codeUrl: 'https://github.com/yourusername/project-repo',
+    title: 'My-CV',
+    description: 'On-going.',
+    // imageUrl: '../../assets/image/mycv.PNG',
+    codeUrl: 'https://github.com/lloydmuncal/myCV',
   });
   </script>
   
